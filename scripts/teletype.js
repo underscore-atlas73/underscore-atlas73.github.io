2242ad55722cbdb02502f13f95f54be421e226bb
AtlasOneStudios <95544912+AtlasOneStudios@users.noreply.github.com>
2026-03-10 21:50:04 -0400

    Initial website

---
+++ scripts/teletype.js
@@ -0,0 +1,28 @@
+const teletypeSpeed = 16;
+async function teletype() {
+    const elements = document.getElementsByClassName("mainTeletype");
+    for (const element of elements) {
+        let text = element.getAttribute("text");
+        for (let i = 0; i < text.length; i++) {
+            await sleep(teletypeSpeed);
+            if ("\\SPEC" == text.substring(i, i+5)) {
+                i += 5;
+                let spec = element.getAttribute("SPEC" + text[i]);
+                element.innerHTML += spec;
+                continue;
+            }
+            if ('\\' === text[i] && 'n' === text[i + 1]) {
+                element.innerHTML += "<br>";
+                i += 1;
+                continue;
+            }
+            element.innerText += text[i];
+        }
+    }
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+document.addEventListener('DOMContentLoaded', teletype);
